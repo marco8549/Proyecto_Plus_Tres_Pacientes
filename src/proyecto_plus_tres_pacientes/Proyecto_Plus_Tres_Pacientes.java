fd5442a29d5d93959c35112d0ead33337c651339
Everest649 <anosmoreno123@gmail.com>
2026-05-15 09:05:15 +0200

    Se crea el metodo para comprobar el codigo de producto y registrar el articulo, y se despliega la vista de registrar articulos

---
+++ src/proyecto_plus_tres_pacientes/Proyecto_Plus_Tres_Pacientes.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_plus_tres_pacientes;
+
+/**
+ *
+ * @author Marco Antonio
+ */
+public class Proyecto_Plus_Tres_Pacientes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
